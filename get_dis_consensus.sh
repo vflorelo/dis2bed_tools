6fdeb3dc12fe0c0a5d652c7b8b2ee750adc37da1
Victor Flores <vflorelo@gmail.com>
2024-09-11 17:30:53 +0100

    debugging

--- get_dis_consensus.sh
+++ get_dis_consensus.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-bed_fof=$1
-base_name=$2
-cutoff=$3
-bed_num=$(cat ${bed_fof} | sort -V | uniq | grep -v ^$ | wc -l)
-min_cons=$(echo "${bed_num}" | awk '{print int($1)+1}')
+base_name=$1
+bed_fof=$2
+min_len=$3
+min_count=$4
 bed_list=$(cat ${bed_fof} | sort -V | uniq | grep -v ^$)
 multiIntersectBed -i $(echo ${bed_list}) > ${base_name}.consensus.tmp.bed
-for i in $(seq ${min_cons} ${bed_num})
-do
-    awk -v i="${i}" 'BEGIN{FS="\t";OFS="\t"}{if($4>=i){print $1,$2,$3,$5}}' ${base_name}.consensus.tmp.bed > ${base_name}.cons_${i}.tmp.bed
-    mergeBed -c 4 -o collapse -delim "|" -i ${base_name}.cons_${i}.tmp.bed | awk -v cutoff="${cutoff}" 'BEGIN{FS="\t"}{if(($3-$2)>=cutoff){print $0}}' > ${base_name}.cons_${i}.bed
-done
+awk -v min_count="${min_count}" 'BEGIN{FS="\t";OFS="\t"}{if($4>=min_count){print $1,$2,$3,$5}}' ${base_name}.consensus.tmp.bed > ${base_name}.consensus.${min_count}.tmp.bed
+mergeBed -c 4 -o collapse -delim "|" -i ${base_name}.consensus.${min_count}.tmp.bed | awk -v min_len="${min_len}" 'BEGIN{FS="\t"}{if(($3-$2)>=min_len){print $0}}' > ${base_name}.consensus.${min_count}.bed
+rm ${base_name}.consensus.tmp.bed ${base_name}.consensus.${min_count}.tmp.bed
